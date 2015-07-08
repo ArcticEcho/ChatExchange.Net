4890810808361b8a634301d32f57cfb5472f3ef9
Sam <ArcticEcho@users.noreply.github.com>
2015-07-08 13:28:24 +0100

    Automated deployment: take 2

--- NuGet/pack.ps1
+++ NuGet/pack.ps1
@@ -4,9 +4,9 @@ $versionStr = "{0}.{1}.{2}-beta" -f ($version.Major, $version.Minor, $version.Re
 
 Write-Host "Setting .nuspec version tag to $versionStr"
 
-$content = (Get-Content $root\NuGet\ChatExchange.Net.nuspec) 
+$content = (Get-Content $root\nuget\ChatExchange.Net.nuspec) 
 $content = $content -replace '\$version\$',$versionStr
 
 $content | Out-File $root\nuget\ChatExchange.Net.compiled.nuspec
 
-& $root\NuGet\NuGet.exe pack $root\nuget\ChatExchange.Net.compiled.nuspec
+& $root\nuget\NuGet.exe pack $root\nuget\ChatExchange.Net.compiled.nuspec
